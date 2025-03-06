a0e10159cfaf17a8b66667b99388ec96f9b623b1
Jônatas <jonatasthome04@gmail.com>
2025-03-06 15:57:43 -0300

    first commit

---
+++ src/main/java/com/devsuperior/exercicio1/Exercicio1Application.java
@@ -0,0 +1,20 @@
+package com.devsuperior.exercicio1;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercicio1Application implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exercicio1Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		
+	}
+
+}
